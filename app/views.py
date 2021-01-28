b908d00112143bb5a0da1ffa775f073b8ea40154
yarensever <45735446+yarensever@users.noreply.github.com>
2021-01-28 15:15:12 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -7,16 +7,16 @@ from numpy import concatenate
 import numpy as np
 import pandas as pd
 import math
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import mean_squared_error
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.layers import LSTM, Bidirectional, GRU
-from keras.layers.recurrent import LSTM
-from sklearn.utils import shuffle
-import requests
-import json
+#from sklearn.preprocessing import MinMaxScaler
+#from sklearn.preprocessing import LabelEncoder
+#from sklearn.metrics import mean_squared_error
+#from keras.models import Sequential
+#from keras.layers import Dense, Dropout, Activation
+#from keras.layers import LSTM, Bidirectional, GRU
+#from keras.layers.recurrent import LSTM
+#from sklearn.utils import shuffle
+#import requests
+#import json
 
 
 scaler  = MinMaxScaler(feature_range=(0, 1))
