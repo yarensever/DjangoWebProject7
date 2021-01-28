6a8226f0f05cfb14a7aa93b1c82a4b92c84925bf
yarensever <45735446+yarensever@users.noreply.github.com>
2021-01-29 01:04:18 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -180,7 +180,7 @@ def home(request):
     mcp_tl  = ar(df_tl)
 
     Saat = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
-    date = today.strftime('%d-%m-%Y')
+    date = tomorrow.strftime('%d-%m-%Y')
     return render(
         request,
         'app/index.html',
